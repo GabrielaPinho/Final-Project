9a70127cfca34d68817631d5841e161027812407
GabrielaPinho <gabrielapinho@g.ucla.edu>
2015-03-19 19:48:54 -0700

    made the 'if' statements more readable, made the function more concise

--- function_get_uniqueGenes.py
+++ function_get_uniqueGenes.py
@@ -8,20 +8,19 @@ def get_uniqueGenes (alltables, name_output_table, name_column):
     Final_list = pd.DataFrame() # creates an empty Dataframe
     Number_genes_per_table = []
     Initial_size = []
-    if alltables == str(alltables): # if only one table was used as input
+    if type(alltables) == str: # if only one table was used as input
         table = pd.read_csv(alltables, header = 0)
         Final_list_noduplicates = table.drop_duplicates([name_column]).dropna()
         print "Only one table was analyzed. Initial length the table: " + str(len(table[name_column])) + ". Unique genes in the list: " + str(len(Final_list_noduplicates)) + "."
 
-    if alltables != str(alltables): # if several tables are being compared and analyzed
+    if type(alltables) != str: # if several tables are being compared and analyzed
         for table in alltables:
             table = pd.read_csv(table, header = 0)
             Initial_size.append(len(table[name_column]))
-            SingleGenes = table.drop_duplicates([name_column]) # the output is all your table without duplicates
-            NoNA = SingleGenes.dropna() # exclude NAs
-            Number_genes_per_table.append(len(NoNA))
-            Final_list = Final_list.append(NoNA)
-        Final_list_noduplicates = Final_list.drop_duplicates([name_column]).dropna() # to exclude genes repeated among lists    
+            SingleGenes = table.drop_duplicates([name_column]).dropna() # the output is all your table without duplicates
+            Number_genes_per_table.append(len(SingleGenes))
+            Final_list = Final_list.append(SingleGenes)
+        Final_list_noduplicates = Final_list.drop_duplicates([name_column]) # to exclude genes repeated among lists    
         print "Analyzed tables: "+ str(alltables) +". Initial lengths of tables: " + str(Initial_size) + ". Unique genes per list: " + str(Number_genes_per_table) + ". Size of the final list is: "+ str(len(Final_list_noduplicates)) + "."
 
     Final_list_noduplicates.to_csv('{}.csv'.format(name_output_table), index = False) # index=True would write the name of the rows
