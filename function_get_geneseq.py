5ffaf3cc649c0a285d14250495a7da4022b17623
GabrielaPinho <gabrielapinho@g.ucla.edu>
2015-03-19 19:45:33 -0700

    made the fuction work for any species, added mike's suggestion to put 'No_seq' instead of 'error' when the command dont find a sequence, added another 'if and else' to account for the possibility of the regular expression not matching with the geneID and made some small changes to make the fuction more readable

--- function_get_geneseq.py
+++ function_get_geneseq.py
@@ -5,41 +5,45 @@ from pandas import DataFrame, read_csv
 import pandas as pd
 import sys
 
-# This function uses the database of Ensemble via MySQL server. From information about gene symbol or gene ID, it can find the gene sequence for a large dataset. It can easily be modified to find other information about the gene, as description or gene type. It uses three arguments: input_table (a string of the name of the file to be analyzed), name_output_table (a string of the prefered name for the output file), name_column (a string of the column name where there is information about Ensemble gene symbol or gene ID), head (0 if the table has header, False if it does not, default = 0), Rel (release of the genome).
+# This function uses the database of Ensemble via MySQL server. From information about gene symbol or gene ID, it can find the gene sequence for a large dataset. It can easily be modified to find other information about the gene, as description or gene type. It uses three arguments: input_table (a string of the name of the file to be analyzed), name_output_table (a string of the prefered name for the output file), name_column (a string of the column name where there is information about Ensemble gene symbol or gene ID), head (0 if the table has header, False if it does not, default = 0), Rel (release of the genome), species (the name of the species in which the gene information will be found, it needs to be a name accepted by Ensembl, example taeniopygia_guttata).
 
 # information about the curl is in http://rest.ensembl.org/documentation/info/xref_external
 
 
-def get_geneseq (input_table, name_output_table, name_column, head = 0, Rel):
+def get_geneseq (input_table, name_output_table, name_column, species, Rel, head = 0):
     table = pd.read_csv(input_table, header = head)
-    genome = Genome(Species='Zebra finch', Release= Rel, account=None)#information about the release searched in the ensemble website
+    genome = Genome(Species='{}'.format(species), Release= Rel, account=None)#information about the release searched in the ensemble website
     Seq = []
     StableID = []
     for GeneSymbol in table[name_column]:
-        if re.search("ENST[A-Z 0-9]*", GeneSymbol) != None:
+        if re.search("ENS[A-Z 0-9]*", GeneSymbol) != None:
             Gene = genome.getGeneByStableId(StableId = GeneSymbol)
             if str(Gene) == 'None':
-                Seq.append("error")
-                StableID.append("error")
+                Seq.append("No_seq")
+                StableID.append("No_ID")
             else:
                 Seq.append(str(Gene.Seq))
                 StableID.append(str(Gene.StableId))
 
-        if re.search("ENST[A-Z 0-9]*", GeneSymbol) == None:
-            GeneralInfo = pexpect.run("curl 'http://rest.ensembl.org/xrefs/symbol/taeniopygia_guttata/{}?' -H 'Content-type:application/json'".format(GeneSymbol))
+        if re.search("ENS[A-Z 0-9]*", GeneSymbol) == None:
+            GeneralInfo = pexpect.run("curl 'http://rest.ensembl.org/xrefs/symbol/{}/{}?' -H 'Content-type:application/json'".format(species, GeneSymbol))
             if str(GeneralInfo) != '[]':
                 ID_re = re.search('.id.:.([\w\d]*).', GeneralInfo)
-                geneID = ID_re.group(1)
-                Gene = genome.getGeneByStableId(StableId = geneID)
-                Seq.append(str(Gene.Seq))
-                StableID.append(str(Gene.StableId))
+                if ID_re == None:
+                    Seq.append("No_seq")
+                    StableID.append("No_ID")
+                else:
+                    geneID = ID_re.group(1)
+                    Gene = genome.getGeneByStableId(StableId = geneID)
+                    Seq.append(str(Gene.Seq))
+                    StableID.append(str(Gene.StableId))
             else:
-                Seq.append("error")
-                StableID.append("error")
+                Seq.append("No_seq")
+                StableID.append("No_ID")
 
-    input_table['StableID'] = StableID
-    input_table['DNAseq'] = Seq
-    input_table.to_csv('{}.csv'.format(name_output_table), index=False, header=True)
+    table['StableID'] = StableID
+    table['DNAseq'] = Seq
+    table.to_csv('{}.csv'.format(name_output_table), index=False, header=True)
     print ("Done!")
 
 
